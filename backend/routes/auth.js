95d660c66ee22611a5ef7d7d548d17e4b3066a9c
anisoara1 <todea.anisoara@gmail.com>
2026-03-26 05:57:27 +0200

    update

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,20 +1,9 @@
 import express from "express";
-import {
-  login,
-  createAdmin,
-  deleteAdmin,
-  resetAdminPassword,
-} from "../controllers/authController.js";
+import { login } from "../controllers/authController.js";
 
 const router = express.Router();
 
 // LOGIN
 router.post("/login", login);
-
-// CREATE ADMIN (temporar)
-router.get("/create-admin", createAdmin);
-
-router.get("/delete-admin", deleteAdmin);
-router.get("/reset-admin", resetAdminPassword);
 
 export default router;
